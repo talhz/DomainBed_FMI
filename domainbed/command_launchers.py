0258599fbbd8bac30e6fd1d81f6a1b77d96e737c
talhz <talhz@pku.edu.cn>
2024-05-05 18:28:50 -0400

    add slurm_launcher

--- domainbed/command_launchers.py
+++ domainbed/command_launchers.py
@@ -10,6 +10,7 @@ import subprocess
 import time
 import torch
 import os
+from multiprocessing import Pool
 
 def local_launcher(commands):
     """Launch commands serially on the local machine."""
@@ -55,11 +56,49 @@ def multi_gpu_launcher(commands):
     for p in procs_by_gpu:
         if p is not None:
             p.wait()
+            
+def slurm_launcher(commands):
+    """
+    Parallel job launcher for computational cluster using SLURM workload manager.
+    An example of SBATCH options:
+        #!/bin/bash
+        #SBATCH --job-name=<job_name>
+        #SBATCH --output=<job_name>.out
+        #SBATCH --error=<job_name>_error.out
+        #SBATCH --ntasks=4
+        #SBATCH --cpus-per-task=8
+        #SBATCH --gres=gpu:4
+        #SBATCH --time=1-00:00:00
+        #SBATCH --mem=81Gb
+    Note: --cpus-per-task should match the N_WORKERS defined in datasets.py (default 8)
+    Note: there should be equal number of --ntasks and --gres
+    @jc-audet
+    """
+
+    with Pool(processes=int(os.environ["SLURM_NTASKS"])) as pool:
+
+        processes = []
+        for command in commands:
+            print(command)
+            process = pool.apply_async(
+                subprocess.run, 
+                [f'{command}'], 
+                {"shell": True}
+                )
+            processes.append(process)
+            time.sleep(10)
+
+        for i, process in enumerate(processes):
+            process.wait()
+            print("//////////////////////////////")
+            print("//// Completed ", i , " / ", len(commands), "////")
+            print("//////////////////////////////")
 
 REGISTRY = {
     'local': local_launcher,
     'dummy': dummy_launcher,
-    'multi_gpu': multi_gpu_launcher
+    'multi_gpu': multi_gpu_launcher, 
+    'slurm_launcher': slurm_launcher
 }
 
 try:
